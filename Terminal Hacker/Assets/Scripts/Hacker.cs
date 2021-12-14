f6893a29b46c76a001ff7838fb512c23d581c354
internetkun <internetkundev@gmail.com>
2021-12-14 10:26:41 +0200

    Random Password
    Setting up the game configuration data and random password generation

--- Terminal Hacker/Assets/Scripts/Hacker.cs
+++ Terminal Hacker/Assets/Scripts/Hacker.cs
@@ -1,14 +1,17 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    // Game configuration data
+    string[] level1Passwords = { "onion", "sheep", "tractor", "plow", "corn", "pasture" };
+    string[] level2Passwords = { "brick", "concrete", "asphalt", "excavator", "bulldozer", "hammer" };
+    string[] level3Passwords = { "hereditary", "ecosystem", "prokariot", "bacteria", "molecule", "nucleus" };
+
     // Game State 
     int level;
     enum Screen { MainMenu, Password, Win };
     Screen currentScreen;
+    string password;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,11 @@ public class Hacker : MonoBehaviour
         ShowMainMenu();
     }
 
+    private void Update()
+    {
+        // int index = Random.Range(0, level1Passwords.Length);
+        // print(index);
+    }
     void ShowMainMenu()
     {
         level = 0;
@@ -38,40 +46,62 @@ public class Hacker : MonoBehaviour
         {
             RunMainMenu(input);
         }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
 
     }
 
     void RunMainMenu(string input)
     {
-        if (input == "1")
+        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+        if (isValidLevelNumber)
         {
-            level = 1;
+            level = int.Parse(input);
             StartGame();
         }
-        else if (input == "2")
-        {
-            level = 2;
-            StartGame();
-        }
-        else  if (input == "3")
-        {
-            level = 3;
-            StartGame();
-        }
-        else  if (input == "GREYGOO")
+        else  if (input == "GREYGOO")  // easter egg
         {
             Terminal.WriteLine("Welcome Goo");
         }
         else
         {
-            Terminal.WriteLine("Waa waa waa, Wrong answer");
+            Terminal.WriteLine("Please select a valid level");
         }
     }
 
     void StartGame()
     {
-        Terminal.WriteLine("You have chosen level " + level);
-        Terminal.WriteLine("Please enter your password: ");
         currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        switch(level)
+        {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
+        Terminal.WriteLine("Please enter your password: ");
+    }
+
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            Terminal.WriteLine("You are correct! Yay!");
+        }
+        else
+        {
+            Terminal.WriteLine("Waa waa waa, Wrong answer. Try again!");
+        }
     }
 }
